f23898617887b000aae362cc463f37c4978d955d
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-04 08:41:34 -0800

    Add dependencies.

---
+++ src/main/java/com/lambdaschool/javashoppingcart/JavaShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javashoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShoppingcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaShoppingcartApplication.class, args);
+    }
+
+}
